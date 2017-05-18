85e55015f07e0a4c66a4d2262842a1f33eb038b4
shtoone2017 <shtoone@shanghaitongwangs-iMac.local>
2017-05-18 11:20:22 +0800

    新闻平台单点登陆

--- toone/Classes/Business/登录/ViewController.m
+++ toone/Classes/Business/登录/ViewController.m
@@ -122,35 +122,39 @@
 //    hud.mode = MBProgressHUDModeCustomView;
 //    hud.label.text = NSLocalizedString(@"正在登录", @"HUD completed title");
     
-    NSString * urlString = [NSString stringWithFormat:AppLogin_2,_acountTextField.text,_passwordTextField.text];
+    NSString * urlString = [NSString stringWithFormat:AppLogin,_acountTextField.text,_passwordTextField.text];
     [[HTTP shareAFNNetworking] requestMethod:GET urlString:urlString parameter:nil success:^(id json) {
         if ([json isKindOfClass:[NSDictionary class]]) {
-            if ([json[@"success"] boolValue]) {
-                //数据存储到本地
-                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                    UserDefaultsSetting  * setting = [UserDefaultsSetting shareSetting] ;
-                    setting.acount = _acountTextField.text;
-                    setting.password = _passwordTextField.text;
-                    setting.departId  = json[@"departId"];
-                    setting.departName  = json[@"departName"];
-                    setting.userPhoneNum = json[@"userPhoneNum"];
-                    setting.userFullName = json[@"userFullName"];
-                    setting.hntchaobiaoReal = json[@"quanxian"][@"hntchaobiaoReal"];
-                    setting.hntchaobiaoSp = json[@"quanxian"][@"hntchaobiaoSp"];
-                    setting.syschaobiaoReal = json[@"quanxian"][@"syschaobiaoReal"];
-                    setting.login = YES;
-                    
-                    
-                    setting.loginDepartId  = json[@"departId"];
-                    setting.userRole  = json[@"userRole"];
-                    [setting saveToSandbox];
-                });
+            NSLog(@"%@",json);
+            return ;
+            if ([json[@"data"] boolValue]) {
+//                //数据存储到本地
+//                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//                    UserDefaultsSetting  * setting = [UserDefaultsSetting shareSetting] ;
+//                    setting.acount = _acountTextField.text;
+//                    setting.password = _passwordTextField.text;
+//                    setting.departId  = json[@"departId"];
+//                    setting.departName  = json[@"departName"];
+//                    setting.userPhoneNum = json[@"userPhoneNum"];
+//                    setting.userFullName = json[@"userFullName"];
+//                    setting.hntchaobiaoReal = json[@"quanxian"][@"hntchaobiaoReal"];
+//                    setting.hntchaobiaoSp = json[@"quanxian"][@"hntchaobiaoSp"];
+//                    setting.syschaobiaoReal = json[@"quanxian"][@"syschaobiaoReal"];
+//                    setting.login = YES;
+//                    
+//                    
+//                    setting.loginDepartId  = json[@"departId"];
+//                    setting.userRole  = json[@"userRole"];
+//                    [setting saveToSandbox];
+//                });
                 
                 //界面跳转
                 dispatch_async(dispatch_get_main_queue(), ^{
-                    id vc = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateInitialViewController];
-                    [UIApplication sharedApplication].keyWindow.rootViewController = vc;
-                    [[UIApplication sharedApplication].keyWindow.layer addTransitionWithType:@"rippleEffect"];
+//                    id vc = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateInitialViewController];
+//                    [UIApplication sharedApplication].keyWindow.rootViewController = vc;
+//                    [[UIApplication sharedApplication].keyWindow.layer addTransitionWithType:@"rippleEffect"];
+                    
+                    
                 });
             }else{
                 hud.mode = MBProgressHUDModeText;
