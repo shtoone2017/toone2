ce79499b96fe0aa30286e745f29e24d9e842ca63
景晓峰 <jingxiaofeng@MacBook-Pro.local>
2017-08-15 16:42:36 +0800

    试验室主页

--- toone/Classes/Business/混凝土-试验室/Inner/Controller/HNT_SYS_InnerController.m
+++ toone/Classes/Business/混凝土-试验室/Inner/Controller/HNT_SYS_InnerController.m
@@ -12,8 +12,11 @@
 #import "HNT_WNSY_Controller.h"
 #import "HNT_TJFX_Controller.h"
 @interface HNT_SYS_InnerController ()
-@property (nonatomic,strong) UIViewController * vc;
-@property (nonatomic,assign) int index;
+
+@property (nonatomic,strong) UIViewController * currentVC;
+
+@property (nonatomic,strong) NSArray *vcsArr;
+
 @end
 
 @implementation HNT_SYS_InnerController
@@ -28,75 +31,73 @@
         [btn3 addTarget:self action:@selector(pan) forControlEvents:UIControlEventTouchUpInside];
         self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:btn3];
     }
-   
-    
-    self.index =1;
-    self.vc = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"HNT_YLSY_Controller"];
-    if ([self.vc isKindOfClass:[HNT_YLSY_Controller class]]) {
-        HNT_YLSY_Controller * controller = (HNT_YLSY_Controller *)self.vc;
-        controller.userGroupId = self.userGroupId;
-        [self addChildViewController:controller];
-        [self.view addSubview:controller.view];
-    }
-    
-     [self loadUI];
+    [self loadUI];
 }
 
 -(void)loadUI{
-    MySegmentedControl * seg = [[NSBundle mainBundle] loadNibNamed:@"MySegmentedControl" owner:nil options:nil][0];
-    seg.frame = CGRectMake(0, 0, 220, 24);
+    NSArray *titles = @[@"压力",@"万能",@"统计",@"配合比",@"通知单"];
+    UISegmentedControl *seg = [[UISegmentedControl alloc] initWithItems:titles];
+    seg.frame = CGRectMake(0,0,220,24);
+    seg.selectedSegmentIndex = 0;
+    seg.tintColor = [UIColor whiteColor];
+    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:[UIColor blackColor],
+                         NSForegroundColorAttributeName,
+                         [UIFont systemFontOfSize:10],
+                         NSFontAttributeName,nil];
+    
+    [seg setTitleTextAttributes:dic forState:UIControlStateSelected];
+    
+    NSDictionary *dic1 = [NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor],
+                          NSForegroundColorAttributeName,
+                          [UIFont systemFontOfSize:10],
+                          NSFontAttributeName,nil];
+    
+    [seg setTitleTextAttributes:dic1 forState:UIControlStateNormal];
+    [seg addTarget:self action:@selector(segmentControlAction:) forControlEvents:UIControlEventValueChanged];
     self.navigationItem.titleView = seg;
-    __weak typeof(self) weakSelf = self;
-    seg.segBlock = ^(int tag){
-        switch (tag) {
-            case 1:{//压力试验
-                if (weakSelf.index !=1){
-                    [weakSelf.vc removeFromParentViewController];
-                    [weakSelf.vc.view removeFromSuperview];
-                    weakSelf.vc = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"HNT_YLSY_Controller"];
-                    if ([weakSelf.vc isKindOfClass:[HNT_YLSY_Controller class]]) {
-                        HNT_YLSY_Controller * controller = (HNT_YLSY_Controller *)weakSelf.vc;
-                        controller.userGroupId = weakSelf.userGroupId;
-                        [weakSelf addChildViewController:controller];
-                        [weakSelf.view addSubview:controller.view];
-                    }
-                    weakSelf.index = 1;
-                }
-                break;
-            }
-            case 2:{
-                //万能试验
-                if (weakSelf.index != 2){
-                    [weakSelf.vc removeFromParentViewController];
-                    [weakSelf.vc.view removeFromSuperview];
-                    weakSelf.vc = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"HNT_WNSY_Controller"];
-                    if ([weakSelf.vc isKindOfClass:[HNT_WNSY_Controller class]]) {
-                        HNT_WNSY_Controller * controller = (HNT_WNSY_Controller *)weakSelf.vc;
-                        controller.userGroupId = weakSelf.userGroupId;
-                        [weakSelf addChildViewController:controller];
-                        [weakSelf.view addSubview:controller.view];
-                    }
-                    weakSelf.index = 2;
-                }
-                break;
-            }
-            case 3:{
-                //统计分析
-                if (weakSelf.index != 3){
-                    [weakSelf.vc removeFromParentViewController];
-                    [weakSelf.vc.view removeFromSuperview];
-                    weakSelf.vc = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"HNT_TJFX_Controller"];
-                    if ([weakSelf.vc isKindOfClass:[HNT_TJFX_Controller class]]) {
-                        HNT_TJFX_Controller * controller = (HNT_TJFX_Controller *)weakSelf.vc;
-                        controller.userGroupId = weakSelf.userGroupId;
-                        [weakSelf addChildViewController:controller];
-                        [weakSelf.view addSubview:controller.view];
-                    }
-                    weakSelf.index = 3;
-                }
-                break;
-            }
-        }
-    };
+    
+    HNT_YLSY_Controller *ylVC = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"HNT_YLSY_Controller"];
+    [self addChildViewController:ylVC];
+    
+    HNT_WNSY_Controller *wnVC = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"HNT_WNSY_Controller"];
+    [self addChildViewController:wnVC];
+    
+    HNT_TJFX_Controller *tjVC = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"HNT_TJFX_Controller"];
+    [self addChildViewController:tjVC];
+    _vcsArr = @[ylVC,wnVC,tjVC];
+    
+    //设置默认vc
+    self.currentVC = ylVC;
+    [self.view addSubview:ylVC.view];
 }
+
+- (void)segmentControlAction:(UISegmentedControl *)seg
+{
+    NSInteger _currentSegIndex = [seg selectedSegmentIndex];
+    [self replaceFromOldViewController:_currentVC toNewViewController:_vcsArr[_currentSegIndex]];
+}
+
+- (void)replaceFromOldViewController:(UIViewController *)oldVc toNewViewController:(UIViewController *)newVc{
+    /**
+     *  transitionFromViewController:toViewController:duration:options:animations:completion:
+     *  fromViewController    当前显示在父视图控制器中的子视图控制器
+     *  toViewController        将要显示的子视图控制器
+     *  duration                动画时间(这个属性,old friend 了 O(∩_∩)O)
+     *  options              动画效果(渐变,从下往上等等,具体查看API)UIViewAnimationOptionTransitionCrossDissolve
+     *  animations            转换过程中得动画
+     *  completion            转换完成
+     */
+    [self addChildViewController:newVc];
+    [self transitionFromViewController:oldVc toViewController:newVc duration:0.1 options:UIViewAnimationOptionTransitionCrossDissolve animations:nil completion:^(BOOL finished) {
+        if (finished) {
+            [newVc didMoveToParentViewController:self];
+            [oldVc removeFromParentViewController];
+            self.currentVC = newVc;
+        }else{
+            self.currentVC = oldVc;
+        }
+    }];
+}
+
+
 @end
