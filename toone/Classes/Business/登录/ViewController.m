dc00e5930573a68a4e2f17948fd7ffc5f4957120
shtoone2017 <shtoone@shanghaitongwangs-iMac.local>
2017-11-10 17:31:19 +0800

    广东项目初始化

--- toone/Classes/Business/登录/ViewController.m
+++ toone/Classes/Business/登录/ViewController.m
@@ -40,12 +40,12 @@
     
     //
     
-//    if ([UserDefaultsSetting shareSetting].isLogin) {
-//        self.acountTextField.text = [UserDefaultsSetting shareSetting].acount;
-//        self.passwordTextField.text = [UserDefaultsSetting shareSetting].password;
-    if ([UserDefaultsSetting_SW shareSetting].isLogin) {
-        self.acountTextField.text = [UserDefaultsSetting_SW shareSetting].acount;
-        self.passwordTextField.text = [UserDefaultsSetting_SW shareSetting].password;
+    if ([UserDefaultsSetting shareSetting].isLogin) {
+        self.acountTextField.text = [UserDefaultsSetting shareSetting].acount;
+        self.passwordTextField.text = [UserDefaultsSetting shareSetting].password;
+//    if ([UserDefaultsSetting_SW shareSetting].isLogin) {
+//        self.acountTextField.text = [UserDefaultsSetting_SW shareSetting].acount;
+//        self.passwordTextField.text = [UserDefaultsSetting_SW shareSetting].password;
 //        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1ull*NSEC_PER_SEC), dispatch_get_main_queue(), ^{
 //            [self loginBtnClick:self.loginButton];
 //        });
@@ -133,34 +133,23 @@
             if ([json[@"success"] boolValue]) {
                 //数据存储到本地
                 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-//                    UserDefaultsSetting  * setting = [UserDefaultsSetting shareSetting] ;
-//                    setting.acount = _acountTextField.text;
-//                    setting.password = _passwordTextField.text;
-//                    setting.departId  = json[@"departId"];
-//                    setting.departName  = json[@"departName"];
-//                    setting.userPhoneNum = json[@"userPhoneNum"];
-//                    setting.userFullName = json[@"userFullName"];
-//                    setting.hntchaobiaoReal = json[@"quanxian"][@"hntchaobiaoReal"];
-//                    setting.hntchaobiaoSp = json[@"quanxian"][@"hntchaobiaoSp"];
-//                    setting.syschaobiaoReal = json[@"quanxian"][@"syschaobiaoReal"];
-//                    setting.login = YES;
-//                    
-//                    
-//                    setting.loginDepartId  = json[@"departId"];
-//                    setting.userRole  = json[@"userRole"];
-//                    [setting saveToSandbox];
-                    
-                    UserDefaultsSetting_SW  * setting = [UserDefaultsSetting_SW shareSetting] ;
+                    UserDefaultsSetting  * setting = [UserDefaultsSetting shareSetting] ;
                     setting.acount = _acountTextField.text;
                     setting.password = _passwordTextField.text;
-                    setting.userType = json[@"userType"];
-                    setting.biaoshi = json[@"biaoshi"];
-                    setting.shenehe = Format(json[@"shenehe"]);
-                    setting.chuzhi = Format(json[@"chuzhi"]);
-                    setting.zxdwshenhe = Format(json[@"zxdwshenhe"]);
+                    setting.departId  = json[@"departId"];
+                    setting.departName  = json[@"departName"];
+                    setting.userPhoneNum = json[@"userPhoneNum"];
                     setting.userFullName = json[@"userFullName"];
+                    setting.hntchaobiaoReal = json[@"chuzhi"];
+                    setting.hntchaobiaoSp = json[@"shenehe"];
+//                    setting.syschaobiaoReal = json[@"syschaobiaoReal"];
                     setting.login = YES;
+                    
+                    
+                    setting.loginDepartId  = json[@"departId"];
+                    setting.userRole  = json[@"userRole"];
                     [setting saveToSandbox];
+                    
                 });
                 
                 //界面跳转
