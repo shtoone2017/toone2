32e1740700f889eaa485749966703e7ab29a7711
shtoone2017 <shtoone@shanghaitongwangs-iMac.local>
2018-01-08 10:59:16 +0800

    初始化。。。

--- toone/Classes/Business/抽屉/DrawerController.m
+++ toone/Classes/Business/抽屉/DrawerController.m
@@ -99,12 +99,12 @@
         });
     }
     
-    if(EqualToString(@"版本", drawer.title)){
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 200ull*NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
-            NSString * address = @"https://www.pgyer.com/DxPd";
-            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:address]];
-        });
-    }
+//    if(EqualToString(@"版本", drawer.title)){
+//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 200ull*NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
+//            NSString * address = @"https://www.pgyer.com/DxPd";
+//            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:address]];
+//        });
+//    }
 }
 
 -(void)dealloc{
